b1c98eba23c58ba2bf14cd1495920daa7b0a164d
robertvunabandi <rvunabandi@gmail.com>
2018-11-12 16:21:14 -0500

    added generate page

--- public/src/components/GeneratePage.jsx
+++ public/src/components/GeneratePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {IDS, CLASSES} from "../variables/identifiers.jsx";
 import "../styles/generate_page.css";
+import GeneratorToolContainer from "./GeneratorTool.jsx";
 
 export default class GeneratePageContainer extends React.Component {
   render() {
@@ -11,7 +12,15 @@ export default class GeneratePageContainer extends React.Component {
 function GeneratePageView() {
   return (
     <div id={IDS.generatePage} className={CLASSES.pageWrapper}>
-      This is upcoming.
+      <h1>MSC Text Generator</h1>
+      <div className={CLASSES.predictIntroTextWrapper}>
+        Generating a (likely) popular confession with some seed text
+      </div>
+      <GeneratorToolContainer />
+      <div className={CLASSES.predictTextWrapper}>
+        The text generated in generated in such a way that it tries to create a popular confession
+        (something that will yield many reactions on the MIT Confessions facebook page).
+      </div>
     </div>
   );
 }
---
+++ public/src/components/GeneratorTool.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {isStringEmpty} from "../utilities/utils.jsx";
+import API from "../utilities/api.jsx";
+import {CLASSES} from "../variables/identifiers.jsx";
+
+
+const DEFAULT_LENGTH = 20;
+
+export default class GeneratorToolContainer extends React.Component {
+  static get defaultState() {
+    return {
+      seed_text: null,
+      length: null,
+      cache: {}, // object mapping from text input to text prediction
+      text_prediction: null,
+    };
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = GeneratorToolContainer.defaultState;
+  }
+
+  predict() {
+    // don't do anything if no string has been given
+    if (isStringEmpty(this.state.seed_text)) {
+      console.warn("no text given");
+      return;
+    }
+
+    // if the result is in the cache, just set that as the prediction
+    if ((this.state.seed_text + this.state.length) in this.state.cache) {
+      const text_prediction = this.state.cache[this.state.text];
+      this.setState(_ => {
+        return {text_prediction};
+      });
+      return;
+    }
+
+    // make the API call
+    API.get(
+      "/api/generate",
+      {seed_text: this.state.seed_text, length: (this.state.length || DEFAULT_LENGTH) + ""},
+      this.predictionSuccessCallback.bind(this),
+      this.predictionFailureCallback.bind(this)
+    );
+  }
+
+  predictionSuccessCallback(data) {
+    this.updateCache(data);
+    this.setState(_ => {
+      return {text_prediction: data};
+    });
+  }
+
+  predictionFailureCallback(error) {
+    // todo - implement this method
+    console.error(error);
+  }
+
+  updateCache(data) {
+    this.setState((prevState, _) => {
+      const new_cache = Object.keys(prevState.cache).reduce((new_cache_, key) => {
+        new_cache_[key] = prevState.cache[key];
+        return new_cache_;
+      }, {});
+      new_cache[prevState.text + prevState.length] = data;
+      return {cache: new_cache};
+    });
+  }
+
+  updateSeedText(event) {
+    this.setState({seed_text: event.target.value});
+  }
+
+  updateLength(event) {
+    this.setState({length: event.target.value});
+  }
+
+  render() {
+    return (
+      <GeneratorToolView
+        seed_text={this.state.seed_text}
+        length={this.state.length}
+        text_prediction={this.state.text_prediction}
+        predict={this.predict.bind(this)}
+        updateSeedText={this.updateSeedText.bind(this)}
+        updateLength={this.updateLength.bind(this)}
+      />
+    );
+  }
+}
+
+function GeneratorToolView(props) {
+  return (
+    <div>
+      <div className={CLASSES.textAreaPredictWrapper}>
+        <textarea
+          value={props.seed_text || ""}
+          onChange={props.updateSeedText}
+          onBlur={props.updateSeedText}/>
+        <br/>
+        <span className={CLASSES.generatorLength}>
+        <span>
+          <span className={CLASSES.generatorLengthLabel}>Length</span>
+          <input
+            className={CLASSES.generatorLengthInput}
+            type={"number"}
+            value={props.length || DEFAULT_LENGTH}
+            maxLength={200}
+            onChange={props.updateLength}
+            onBlur={props.updateLength}/>
+        </span>
+        </span>
+      </div>
+      <div className={CLASSES.buttonPredictWrapper}>
+        <button onClick={props.predict}>Predict</button>
+      </div>
+      <div className={CLASSES.generatorPrediction}>
+        <span>{props.text_prediction}</span>
+      </div>
+    </div>
+  );
+}
+
+GeneratorToolView.propTypes = {
+  seed_text: PropTypes.string,
+  length: PropTypes.number,
+  text_prediction: PropTypes.string,
+  predict: PropTypes.func.isRequired,
+  updateSeedText: PropTypes.func.isRequired,
+  updateLength: PropTypes.func.isRequired,
+};
