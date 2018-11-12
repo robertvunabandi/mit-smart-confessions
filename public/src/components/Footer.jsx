cc4260b8abda0a1529c830b570694ff090539d7a
robertvunabandi <rvunabandi@gmail.com>
2018-11-12 17:20:42 -0500

    making the footer stick to the bottom of the page

--- public/src/components/Footer.jsx
+++ public/src/components/Footer.jsx
@@ -5,11 +5,33 @@ import {uppercaseFirstLettersSimpleString} from "../utilities/utils.jsx";
 
 const FOOTER_OPTIONS = [
   { name: "privacy", url: "/privacy" },
-  { name: "about", url: "/about" },
+  { name: "about MIT Smart Confession", url: "/about" },
   { name: "how it works", url: "/how-it-works" },
 ];
 
 class FooterContainer extends React.Component {
+  componentDidMount() {
+    console.log("mounted footer");
+    setTimeout(FooterContainer.updateFooterHeight, 0);
+    setTimeout(FooterContainer.updateFooterHeight, 50);
+    window.addEventListener("resize", FooterContainer.updateFooterHeight);
+  }
+
+  static updateFooterHeight() {
+    const footer = document.querySelector("#" + IDS.footer);
+    const root_init = document.querySelector("#" + IDS.rootInit);
+    const navbar = document.querySelector("#" + IDS.navBar);
+    const footer_height = footer.clientHeight;
+    const above_footer_height = root_init.clientHeight + navbar.clientHeight;
+    const window_height = window.innerHeight;
+    if (above_footer_height + footer_height <= window_height) {
+      const top_to_fill_window_height = window_height - (above_footer_height + footer_height);
+      footer.style.top = top_to_fill_window_height + "px";
+    } else if (above_footer_height + footer_height > window_height) {
+      footer.style.top = null;
+    }
+  }
+
   render() {
     return <FooterView {...this.props} />;
   }
@@ -27,6 +49,7 @@ function FooterView(props) {
           </li>;
         })}
       </ul>
+      <br/>
       Copyright &#9400; 2018-2019 {APP_NAME}
     </footer>
   );
