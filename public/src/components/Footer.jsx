8fd6dc68c2aa7c17d6da402bc61975ec6c55cf71
robertvunabandi <rvunabandi@gmail.com>
2018-11-13 19:46:10 -0500

    added todo in the footer for event handling when predict is called, and refactored the sticky footer algorithm

--- public/src/components/Footer.jsx
+++ public/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {IDS} from "../variables/identifiers.jsx";
 import {APP_NAME} from "../variables/identifiers.jsx";
-import {uppercaseFirstLettersSimpleString} from "../utilities/utils.jsx";
+import {uppercaseFirstLettersSimpleString, isStringEmpty} from "../utilities/utils.jsx";
 
 const FOOTER_OPTIONS = [
   { name: "about MIT Smart Confession", url: "/about" },
@@ -10,18 +10,30 @@ const FOOTER_OPTIONS = [
 
 class FooterContainer extends React.Component {
   componentDidMount() {
-    console.log("mounted footer");
     setTimeout(FooterContainer.updateFooterHeight, 0);
     setTimeout(FooterContainer.updateFooterHeight, 50);
     window.addEventListener("resize", FooterContainer.updateFooterHeight);
+    // todo - add an event handling to update the footer content when a
+    // prediction is made so that the footer updates accordingly, it should
+    // wait a bit before updating
+    // window.addEventListener("prediction-made", FooterContainer.updateFooterHeight);
+  }
+
+  static getCssFooterTop() {
+    const footer = document.querySelector("#" + IDS.footer);
+    // assume the top is written in pixels
+    if (isStringEmpty(footer.style.top)) {
+      return 0;
+    }
+    return parseFloat(footer.style.top.substring(0, footer.style.top.length - 2));
   }
 
   static updateFooterHeight() {
     const footer = document.querySelector("#" + IDS.footer);
-    const root_init = document.querySelector("#" + IDS.rootInit);
-    const navbar = document.querySelector("#" + IDS.navBar);
-    const footer_height = footer.clientHeight;
-    const above_footer_height = root_init.clientHeight + navbar.clientHeight;
+    const footer_top = FooterContainer.getCssFooterTop();
+    const bounding_box = footer.getBoundingClientRect();
+    const above_footer_height = bounding_box.top - footer_top;
+    const footer_height = bounding_box.height;
     const window_height = window.innerHeight;
     if (above_footer_height + footer_height <= window_height) {
       const top_to_fill_window_height = window_height - (above_footer_height + footer_height);
