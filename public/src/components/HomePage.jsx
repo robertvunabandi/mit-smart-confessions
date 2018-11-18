fc58548756e3def12633d954d5665bad2f910b1d
robertvunabandi <rvunabandi@gmail.com>
2018-11-17 20:24:32 -0500

    adding text and breaks (stylistical changed)

--- public/src/components/HomePage.jsx
+++ public/src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import LineBreakContainer from "./LineBreak.jsx";
-import "../styles/home_page.css";
+import "../styles/home_page.scss";
 import {IDS, CLASSES} from "../variables/identifiers.jsx";
 import HorizontalSplitDivContainer from "./HorizontalSplitDiv.jsx";
 
@@ -14,29 +14,35 @@ function HomePageView() {
   return (
     <div id={IDS.homePage} className={CLASSES.pageWrapper} >
       <h1>Welcome to MIT Smart Confession</h1>
+      <br/>
       We use machine learning to take confessions to the next level. Take a look
       at the tools that we offer.
+      <br/>
+      <br/>
       <LineBreakContainer />
       <HorizontalSplitDivContainer>
-        <div>
+        <div className={CLASSES.homePageBlock}>
           <h3>Confession Rater</h3>
           Predict how popular your confession will be.
+          <br />
           <br />
           <a href={"/predict"}>
             Try it
           </a>
         </div>
-        <div>
+        <div className={CLASSES.homePageBlock}>
           <h3>Smart Confession Completion</h3>
           Start writing, let our tool complete the confession to maximize popularity.
+          <br />
           <br />
           <a href={"/generate"}>
             Try it
           </a>
         </div>
-        <div>
+        <div className={CLASSES.homePageBlock}>
           <h3>Understanding Confessions</h3>
           Understand what makes some confessions more popular than others.
+          <br />
           <br />
           <a href={"/understand"}>
             View Explanations
@@ -44,6 +50,7 @@ function HomePageView() {
         </div>
       </HorizontalSplitDivContainer>
       <LineBreakContainer />
+      <br />
       If interested, we can tell you about <a href={"/how-it-works"}>how this works</a> behind
       the scene.
     </div>
